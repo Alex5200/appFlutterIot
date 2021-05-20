9d48dcdbfad0d506892f1bf82dabeb4887901b5b
FlutLab.io <>
2021-05-20 10:53:58 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -61,15 +61,20 @@ class FirstRoute extends StatelessWidget {
                     //btn 2
                     style: btnStyle,
                     onPressed: () {
-                final snackBar = SnackBar(
-                  content: Text('Yay! A SnackBar!'),
-                  action: SnackBarAction(
-                  label: 'Undo',
-                  onPressed: () {
-                    // Some code to undo the change.
-                  },
-                ),
-              );
+                      final snackBar = SnackBar(
+                        content: Text('Yay! A SnackBar!'),
+                        action: SnackBarAction(
+                          label: 'Undo',
+                          onPressed: () {
+                            // Some code to undo the change.
+                          },
+                        ),
+                      );
+
+                      // Find the ScaffoldMessenger in the widget tree
+                      // and use it to show a SnackBar.
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
                     child: const Text(
                       'Submit',
                       style: TextStyle(
