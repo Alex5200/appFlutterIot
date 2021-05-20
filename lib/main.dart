f225e63d5380be32d557b3a335c52504aada63ef
FlutLab.io <>
2021-05-20 11:27:21 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -28,67 +28,71 @@ class FirstRoute extends StatelessWidget {
           title: Text('IoT'),
         ),
         body: Center(
-          child: Container(
-            margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-            child: new Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    //alignment: Alignment(2.0, 2.0),
-                    margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Column(children: <Widget>[
+          Container(),
+          Container(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: new Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      //alignment: Alignment(2.0, 2.0),
+                      margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
 
-                    width: 150,
-                    height: 50,
-                    child: TextButton(
-                        // btn 1
+                      width: 150,
+                      height: 50,
+                      child: TextButton(
+                          // btn 1
+                          style: btnStyle,
+                          onPressed: () {},
+                          child: const Text(
+                            'Loging',
+                            style: TextStyle(
+                              fontSize: 21,
+                              color: Colors.white,
+                            ),
+                          )),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      width: 350,
+                      height: 50,
+                      child: TextButton(
+                        //btn 2
                         style: btnStyle,
-                        onPressed: () {},
+                        onPressed: () {
+                          final snackBar = SnackBar(
+                            content: Text('Hey you loging '),
+                            backgroundColor: Colors.green,
+                            //color: Colors.white,
+                            action: SnackBarAction(
+                              textColor: Colors.white,
+                              label: 'Ok',
+                              onPressed: () {
+                                // Some code to undo the change.
+                              },
+                            ),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        },
                         child: const Text(
-                          'Loging',
+                          'Submit',
                           style: TextStyle(
                             fontSize: 21,
                             color: Colors.white,
                           ),
-                        )),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    width: 350,
-                    height: 50,
-                    child: TextButton(
-                      //btn 2
-                      style: btnStyle,
-                      onPressed: () {
-                        final snackBar = SnackBar(
-                          content: Text('Hey you loging '),
-                          backgroundColor: Colors.green,
-                          //color: Colors.white,
-                          action: SnackBarAction(
-                            textColor: Colors.white,
-                            label: 'Ok',
-                            onPressed: () {
-                              // Some code to undo the change.
-                            },
-                          ),
-                        );
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      },
-                      child: const Text(
-                        'Submit',
-                        style: TextStyle(
-                          fontSize: 21,
-                          color: Colors.white,
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
+        ])),
         drawer: Drawer(
           child: ListView(
             padding: EdgeInsets.zero,
