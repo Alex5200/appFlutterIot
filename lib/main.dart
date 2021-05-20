ce6ce0cb1288009ea82b7e178fd4c8d8725ae17e
FlutLab.io <>
2021-05-20 11:35:42 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class FirstRoute extends StatelessWidget {
               width: 320,
               height: 50,
               child: TextField(
-                textColor: Colors.green,
+                cursorColor: Colors.green,
                 //style: TextField(),
                 decoration: InputDecoration(border: OutlineInputBorder(), hintText: 'Enter a search term'),
               )),
